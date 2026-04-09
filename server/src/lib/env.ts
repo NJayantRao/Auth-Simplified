ad2a49d13c907a366924d59f9e21a65de603a565
N Jayant Rao <njayantrao@gmail.com>
2026-04-09 20:15:36 +0530

    feature: Add postgres & prisma config.

--- server/src/lib/env.ts
+++ server/src/lib/env.ts
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 dotenv.config({ quiet: true });
 
 const ENV = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT!,
+  DATABASE_URL: process.env.DATABASE_URL!,
 };
 
 export { ENV };
