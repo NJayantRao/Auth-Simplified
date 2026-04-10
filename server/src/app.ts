a3981b4462f20f7e259ddab51abdffff64b4c172
N Jayant Rao <njayantrao@gmail.com>
2026-04-11 00:47:28 +0530

    feature: Add ip based rate-limiter

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,11 +4,14 @@ import { authRouter } from "./routes/auth.routes.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger-output.json" with { type: "json" };
 import cookieParser from "cookie-parser";
+import { rateLimiter } from "./middlewares/rate-limit-middleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.set("trust proxy", 1);
+app.use(rateLimiter);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("", rootRouter);
---
+++ server/src/middlewares/rate-limit-middleware.ts
@@ -0,0 +1,22 @@
+import { redisClient } from "../lib/redis.js";
+import ApiError from "../utils/api-error.js";
+
+const rateLimiter = async (req: any, res: any, next: any) => {
+  try {
+    const rateLimitKey = `rate-limit:${req.ip}`;
+    const requests = await redisClient.incr(rateLimitKey);
+    if (requests === 1) {
+      await redisClient.expire(rateLimitKey, 60);
+    } else if (requests > 10) {
+      return res
+        .status(429)
+        .json(new ApiError(429, "Too many requests. Please try again later."));
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(new ApiError(500, "Internal Server Error"));
+  }
+};
+
+export { rateLimiter };
